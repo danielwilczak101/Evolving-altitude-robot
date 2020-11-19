13c96d82a625c534fa535f6c8612f44c62ab7c11
Daniel Wilczak <44122838+danielwilczak101@users.noreply.github.com>
2020-11-19 06:28:01 +0000

    Update new_fitness.py

--- python/new_fitness.py
+++ python/new_fitness.py
@@ -1,9 +1,15 @@
 import EasyGA
-
+import serial
+import time
+import random
 # Create the genetic algorithm
 ga = EasyGA.GA()
 
 ga.gene_impl = lambda: random.randint(1240, 1280)
+ga.generation_goal = 1
+ga.population_size = 4
+ga.chromosome_length = 10
+
 
 # Setup ardunio serial conneciton
 arduino_comm_port = "/dev/ttyACM0"
@@ -12,7 +18,7 @@ ser = serial.Serial(arduino_comm_port, baudrate=9600, timeout=1)
 time.sleep(3)
 
 
-def is_valid(self, sentString, recvString):
+def is_valid(sentString, recvString):
         if sentString in recvString:
             return True
         else:
@@ -20,23 +26,30 @@ def is_valid(self, sentString, recvString):
         # string must be in this format: [1,2,3]{4,5,6}
         # string is received directly from arduino
 
-def get_fitness(chromosome):
-
+def get_fitness(input_value):
+    
+    chromosome = []
+    
+    for gene in input_value.gene_list:
+        chromosome.append(gene.value)
+    
     # Goal Attributes
     max_height = 160
     target_height = 6
     fitness = 0
 
-
     # Encode chromosome into byte data to send to the ardunio
     string = '[' + ','.join(map(str, chromosome)) + ']'
+    
+    print("starting")
     encoded = str.encode(string)
     ser.write(encoded)
+    print()
     print("Sending:")
     print("\t" + str(string))
 
     what_i_just_read = ser.readline().decode()  # Send command
-
+    print(what_i_just_read)
     valid = is_valid(string, what_i_just_read)
     # [1,2,3]{4,5,6}
 
@@ -74,6 +87,10 @@ def get_fitness(chromosome):
     return fitness
 
 
-
+ga.fitness_function_impl = get_fitness
 # Evolve the genetic algorithm
 ga.evolve()
+
+ga.print_generation()
+ga.print_population()
+
