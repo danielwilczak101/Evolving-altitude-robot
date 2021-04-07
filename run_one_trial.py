ac3f4f69900e80f08ca2412c34db22fef68e43f3
danielwilczak101 <44122838+danielwilczak101@users.noreply.github.com>
2021-04-07 02:36:05 -0400

    small changes

--- run_one_trial.py
+++ run_one_trial.py
@@ -6,7 +6,7 @@ import random
 from connection_functions import send_chromosome
 
 # Setup ardunio serial conneciton
-arduino_comm_port = "/dev/ttyACM0"
+arduino_comm_port = "/dev/cu.usbmodem14201"
 ser = serial.Serial(arduino_comm_port, baudrate=9600, timeout=1)
 # Allow the arduino to initialize
 time.sleep(3)
@@ -14,7 +14,7 @@ time.sleep(3)
 def robot_fitness(chromosome):
     """The fitness function that calulates the fitness based
     on the robots findings."""
-    
+
     # Get the global serial communication variable.
     global ser
 
@@ -40,21 +40,24 @@ ga = EasyGA.GA()
 ga.fitness_function_impl = robot_fitness
 
 ga.gene_impl = lambda: random.randint(1250, 1270)
-ga.generation_goal = 5
-ga.population_size = 4
+ga.generation_goal = 100
+ga.population_size = 10
 ga.chromosome_length = 10
 ga.target_fitness_type = 'min'
 
 while ga.active():
     # Evolve only a certain number of generations
     ga.evolve(1)
-    # Print the current generation
-    ga.print_generation()
-    ga.print_population()
-    # Print the best chromosome from that generations population
-    ga.print_best_chromosome()
+    # Print the worst chromosome("Smallest") from that generations population
+    ga.print_worst_chromosome()
     # If you want to show each population
     #ga.print_population()
     # To divide the print to make it easier to look at
     print('-'*75)
 
+# Print the current generation
+ga.print_generation()
+ga.print_population()
+
+ga.graph.lowest_value_chromosome()
+ga.graph.show()
